f7ed6caf2c379d51061732953c5ab4a97d5e767b
ShabeenaBegum21 <shabeenaofficial123@gmail.com>
2026-05-03 12:57:41 +0530

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,1 +1,14 @@
-
+CREATE TABLE jobs (
+    work_year INT,
+    experience_level TEXT,
+    employment_type TEXT,
+    job_title TEXT,
+    salary INT,
+    salary_currency TEXT,
+    salary_in_usd INT,
+    employee_residence TEXT,
+    remote_ratio INT,
+    remote_type TEXT,
+    company_location TEXT,
+    company_size TEXT
+);
